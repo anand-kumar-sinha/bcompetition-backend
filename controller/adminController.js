642a091839d2929e1368b31d7e64ec3773f1dd9c
anand kumar <anandkumar7368970125@gmail.com>
2024-12-31 02:01:37 +0530

    fix add student

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,10 +1,12 @@
-
+const generateReferralCode = require("../middleware/generateRefferalCode");
 const generateToken = require("../middleware/generateToken");
 const Admin = require("../models/Admin");
 const Category = require("../models/Category");
+const City = require("../models/City");
+const Country = require("../models/Country");
+const State = require("../models/State");
 const Test = require("../models/Test");
 const User = require("../models/User");
-
 
 const createAdmin = async (req, res) => {
   try {
@@ -94,11 +96,11 @@ const loginAdmin = async (req, res) => {
   }
 };
 
-const adminDashboard = async (req, res) =>{
+const adminDashboard = async (req, res) => {
   try {
     const categories = await Category.countDocuments();
     const students = await User.countDocuments();
-    const test = await Test.countDocuments({isDeleted: false});
+    const test = await Test.countDocuments({ isDeleted: false });
 
     res.status(200).json({
       success: true,
@@ -111,9 +113,99 @@ const adminDashboard = async (req, res) =>{
       success: false,
       error: error,
     });
-    
   }
-}
+};
+
+const createUser = async (req, res) => {
+  try {
+    const {
+      mobile_number,
+      name,
+      email,
+      schoolname,
+      dob,
+      gender,
+      address,
+      pincode,
+      country,
+      state,
+      city,
+    } = req.body;
+
+    if (
+      !mobile_number ||
+      !name ||
+      !email ||
+      !schoolname ||
+      !dob ||
+      !gender ||
+      !address ||
+      !pincode ||
+      !country ||
+      !state ||
+      !city 
+    ) {
+      res.status(400).json({
+        success: false,
+        message: "Please enter all required fields",
+      });
+      return;
+    }
+
+    const userExists = await User.findOne({ mobile_number });
+    if (userExists) {
+      res.status(400).json({
+        success: false,
+        message: "User with this mobile_number already exists",
+      });
+      return;
+    }
+
+    const countryName = await Country.findById(country);
+    const stateName = await State.findById(state);
+    const cityName = await City.findById(city);
+
+    const referral_code = generateReferralCode();
+
+    const newUser = await User.create({
+      mobile_number,
+      name,
+      email,
+      school_name: schoolname,
+      date_of_birth: dob,
+      gender,
+      address,
+      pincode,
+      country: countryName.country,
+      state: stateName.state,
+      city: cityName.city,
+      referral_code,
+      isRegister: true,
+      wallet_amount: 0,
+      reward_earn: 0,
+      isVerified: true,
+    });
+
+    if (!newUser) {
+      res.status(400).json({
+        success: false,
+        message: "Failed to create user",
+      });
+      return;
+    }
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      error: error.message || error,
+    });
+  }
+};
 
 const adminShut = async (req, res) => {
   process.exit(0);
@@ -128,5 +220,6 @@ module.exports = {
   loginAdmin,
   adminShut,
   adminOn,
-  adminDashboard
+  adminDashboard,
+  createUser
 };
